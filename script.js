95927e99267e4b8caa64aaf59a3a3331da54e4ef
kentaro <chickenshopgogo@gmail.com>
2019-01-23 08:26:09 +0900

    json値変更とnoのclick関数作成

--- script.js
+++ script.js
@@ -51,17 +51,17 @@ window.onload  =  function() {
  
 //JSONーーーーーーーーーーーーーーーーーーーーーーー
 const question1 = '{ "problem" : [' +
-'{ "id": "1","quest": "最近調子がイマイチ良くない。","yes": "2","no": "8" },' +
-'{ "id": "2","quest": "偏食しがち","yes": "3","no": "4" },' +
-'{ "id": "3","quest": "よく目が充血する","yes": "5","no": "4" },' +
-'{ "id": "4","quest": "余り日光にあたらない","yes": "7","no": "6" },' +
-'{ "id": "5","quest": "暗いところでは、よく見えない","yes": "100","no": "7" },' +
-'{ "id": "6","quest": "寝つきが悪い。または、なんども目が覚める。","yes": "12","no": "8" },' +
-'{ "id": "7","quest": "余り魚を食べない","yes": "200","no": "10" },' +
-'{ "id": "8","quest": "肌荒れが気になる","yes": "400","no": "9" },' +
-'{ "id": "9","quest": "怠い。疲れやすい。","yes": "11","no": "1000" },' +
+'{ "id": "1","quest": "最近調子がイマイチ良くない。","yes": "1","no": "7" },' +
+'{ "id": "2","quest": "偏食しがち","yes": "2","no": "3" },' +
+'{ "id": "3","quest": "よく目が充血する","yes": "4","no": "3" },' +
+'{ "id": "4","quest": "余り日光にあたらない","yes": "6","no": "5" },' +
+'{ "id": "5","quest": "暗いところでは、よく見えない","yes": "100","no": "6" },' +
+'{ "id": "6","quest": "寝つきが悪い。または、なんども目が覚める。","yes": "11","no": "7" },' +
+'{ "id": "7","quest": "余り魚を食べない","yes": "200","no": "9" },' +
+'{ "id": "8","quest": "肌荒れが気になる","yes": "400","no": "8" },' +
+'{ "id": "9","quest": "怠い。疲れやすい。","yes": "10","no": "1000" },' +
 '{ "id": "11","quest": "貧血気味である","yes": "600","no": "900" },' +
-'{ "id": "12","quest": "集中力、思考力が低下していると感じる。","yes": "300","no": "10" } ]}';
+'{ "id": "12","quest": "集中力、思考力が低下していると感じる。","yes": "300","no": "9" } ]}';
 
 
 //パースしてる
@@ -76,19 +76,30 @@ yes_number.innerHTML = obj.problem[0].yes;
 no_number.innerHTML = obj.problem[0].no;
 
 
+
 const yes = document.getElementById('yes').onclick = function () {
     yes_number = document.getElementById('yes_number').innerHTML;
     
     question.innerHTML = obj.problem[yes_number].quest;
     let yes_number2 =  obj.problem[yes_number].yes;
+    let no_number2 =  obj.problem[yes_number].no;
     document.getElementById('yes_number').innerHTML = yes_number2;
-    console.log(yes_number2);
-   
-    
+    document.getElementById('no_number').innerHTML = no_number2;
+
 };
 
 const no = document.getElementById('no').onclick = function () {
-    question.innerHTML = obj.problem[noTwo].quest;
+    no_number = document.getElementById('no_number').innerHTML;
+    
+    question.innerHTML = obj.problem[no_number].quest;
+    let yes_number2 =  obj.problem[no_number].yes;
+    let no_number2 =  obj.problem[no_number].no;
+
+    document.getElementById('yes_number').innerHTML = yes_number2;
+    document.getElementById('no_number').innerHTML = no_number2;
+
 };
+
+
 
 }
